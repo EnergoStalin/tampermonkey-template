664f782c85815d37fbca5b4506f8f42d7eb277bb
EnergoStalin <ens.stalin@gmail.com>
2024-01-31 19:40:18 +0300

    chore: namespace == pkg.homepage

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,7 +11,7 @@ const metadata = {
 	description: pkg.description,
 	license: pkg.license,
 	version: pkg.version,
-	namespace: "domain backwards",
+	namespace: pkg.homepage,
 	match: pkg.homepage,
 	"run-at": "document-body",
 	connect: [],
