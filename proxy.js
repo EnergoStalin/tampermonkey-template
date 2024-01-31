88e5d145b72d0a4dab32e4586cc0454f6f102c46
EnergoStalin <ens.stalin@gmail.com>
2024-01-31 18:08:07 +0300

    chore: remove unused files

--- proxy.js
+++
@@ -1,9 +0,0 @@
-(async function() {
-	GM.xmlHttpRequest({
-      method: "GET",
-      url: "http://127.0.0.1:8080/src/bundle.user.js",
-      onload: function(response) {
-          eval(response.responseText);
-      },
-  });
-})();
